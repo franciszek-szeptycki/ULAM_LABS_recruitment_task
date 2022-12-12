f11cf786f214fb01de60921f4547fa50ecd899ea
franciszek.szeptycki <franciszek.szeptycki@gmail.com>
2022-12-12 02:04:21 +0100

    build error fixed

--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -5,7 +5,6 @@ import {
     setInitialCoins,
 } from "../../utils/localStorageManagment";
 import Chart from "../charts/Chart";
-import MyChart from "../charts/MyChart";
 import Header from "../header/Header";
 import Widget from "../widget/Widget";
 import "./Container.sass";
